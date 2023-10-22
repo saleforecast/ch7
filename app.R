d17c4858be163d11d3d3f9d9047dc4e89b983cdb
sidul567 <sidulsam@gmail.com>
2023-10-23 00:03:05 +0600

    First Commit

---
+++ app.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(ggplot2)
+
+ui <- fluidPage(
+  plotOutput("plot", brush="brushPlot", dblclick = "dblClickPlot")
+)
+
+server <- function(input, output, session) {
+  selected <- reactiveVal(rep(FALSE, nrow(mtcars)))
+  
+  observeEvent(input$brushPlot, {
+    selected(brushedPoints(mtcars, input$brushPlot, allRows = TRUE)$selected_ | selected())
+  })
+  
+  observeEvent(input$dblClickPlot, {
+    selected(rep(FALSE, nrow(mtcars)))
+  })
+  
+  output$plot <- renderPlot({
+    mtcars$selected <- selected()
+    ggplot(mtcars, aes(wt, mpg, color = selected)) +
+      geom_point()+
+      scale_color_discrete(limits = c("TRUE", "FALSE"))
+  })
+}
+
+shinyApp(ui, server)
